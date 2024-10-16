0803e5527921ee91e5e4be65077f608c90d65c8b
armlynobinguar <aemyobinguar@gmail.com>
2024-10-16 12:19:07 +0800

    added the rust-fundamentals

---
+++ examples/2-variables/variables/src/main.rs
@@ -0,0 +1,11 @@
+
+
+fn main() {
+    let message = "Name: Alfredo, Weight: ";
+    let weight = 190.0;
+
+    let kilos = weight / 2.2;
+    println!("{}{}", message, weight);
+    println!("{}{}", message, kilos);
+    
+}
