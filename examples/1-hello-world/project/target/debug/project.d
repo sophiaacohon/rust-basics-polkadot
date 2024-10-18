bcecd209fccc557f1c612ac62517d7a07792267b
armlynobinguar <aemyobinguar@gmail.com>
2024-10-18 15:29:53 +0800

    added the modification

--- examples/1-hello-world/project/target/debug/project.d
+++ examples/1-hello-world/project/target/debug/project.d
@@ -1,1 +1,1 @@
-/Users/armielynobinguar/rust-basics-web3-cebu-polkadot/examples/1-components/project/target/debug/project: /Users/armielynobinguar/rust-basics-web3-cebu-polkadot/examples/1-components/project/src/main.rs
+/Users/armielynobinguar/rust-basics-web3-cebu-polkadot/examples/1-hello-world/project/target/debug/project: /Users/armielynobinguar/rust-basics-web3-cebu-polkadot/examples/1-hello-world/project/src/main.rs
