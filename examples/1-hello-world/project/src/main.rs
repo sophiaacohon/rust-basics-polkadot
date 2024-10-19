bac00b342ae54ff21463fb81a750d6118e958aa5
0x42697262 <giogulane.wineffgulane@gmail.com>
2024-10-19 20:01:44 +0900

    Group 22: solution to activty #1

--- examples/1-hello-world/project/src/main.rs
+++ examples/1-hello-world/project/src/main.rs
@@ -3,4 +3,9 @@
 // This is the main function, which is also the main entry point. Libraries may not have a main function at all.
 fn main() {
     println!("Hello, world!");
+    test();
+}
+
+fn test() {
+    println!("We are here.");
 }
